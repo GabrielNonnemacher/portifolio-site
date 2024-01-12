ffcc41f04ebbccaecd1035685f0b7de0d834ac13
Gabriel Nonnemacher <NonnemacherG@delagelanden.com>
2024-01-11 21:16:27 -0300

    Task #1 - reformulando site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,42 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
-import { ButtonHeaderComponent } from './components/button-header/button-header.component';
-import { HomeComponent } from './components/home/home.component';
-import { CertificatesComponent } from './components/certificates/certificates.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ButtonFooterComponent } from './components/button-footer/button-footer.component';
-import { ProjectsComponent } from './components/projects/projects.component';
-import { BoxProjectsComponent } from './components/box-projects/box-projects.component';
-import { ImageSliderComponent } from './components/image-slider/image-slider.component';
-import { AboutComponent } from './components/about/about.component';
-import { ModalComponent } from './components/modal/modal.component';
+import { ComponentModule } from './components/component.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    ButtonHeaderComponent,
-    HomeComponent,
-    CertificatesComponent,
-    FooterComponent,
-    ButtonFooterComponent,
-    ProjectsComponent,
-    BoxProjectsComponent,
-    ImageSliderComponent,
-    AboutComponent,
-    ModalComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatIconModule
+    ComponentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,28 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
-import { INFORMATIONS } from 'src/assets/helpers/Informations';
-import { ButtonHeader, ButtonHeaderDTO } from './models/ButtonHeader.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'Header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
+export class HeaderComponent {
 
-  contentButtonsHeader: ButtonHeader[] = [] as ButtonHeader[];
-  ngUnsubscribe = new Subject<void>();
-
-  public ngOnInit(): void {
-    this.composeButtonsHeader();
-  }
-
-  private composeButtonsHeader(): void {
-    this.contentButtonsHeader = INFORMATIONS.ButtonsHeaderContents.map(ButtonHeaderDTO.mapperListView);
-  }
-
-  public ngOnDestroy(): void {
-    this.ngUnsubscribe.next();
-    this.ngUnsubscribe.complete();
-  }
 }
