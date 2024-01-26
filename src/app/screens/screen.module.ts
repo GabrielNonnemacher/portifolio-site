ed2c5c37b3eef9a25c439d36f3fff2e357d13267
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-01-26 02:00:31 -0300

    Task #12 - ajustando arquivos(about <=> home)

--- src/app/screens/screen.module.ts
+++ src/app/screens/screen.module.ts
@@ -2,19 +2,22 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ComponentModule } from '../components/component.module';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
     ComponentModule
   ],
   exports: [
-    AboutComponent
+    AboutComponent,
+    HomeComponent
   ]
 })
 export class ScreenModule {}
