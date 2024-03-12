fe8ee757f7fdf1bb7018294caff8b45fbc3d8996
Gabriel Nonnemacher <nonnemacher99gabriel@gmail.com>
2024-03-11 22:14:32 -0300

    Task #30 - ajustando layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, HostListener, Input, signal } from '@angular/core';
 import { HEADER_BUTTONS_CONTENT } from 'src/app/helpers/constatants/headerButtonsContent.constant';
 import { HeaderButton } from 'src/app/helpers/models/headerButton.model';
 
@@ -11,6 +11,14 @@ export class HeaderComponent {
   openNavHeaderMobile = signal<boolean>(false);
   firstEnter = signal<boolean>(true);
   buttonsHeader = signal<HeaderButton[]>(HEADER_BUTTONS_CONTENT);
+  isVisible = signal<boolean>(false);
+
+  @HostListener('window:scroll')
+  public onWindowScroll(): void {
+    const alturaAtual = document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const alturaParaAparecer = 80;
+    this.isVisible.set(alturaAtual > alturaParaAparecer);
+  }
 
   public onClickMenu(): void {
     this.openNavHeaderMobile.update(prop => !prop);
